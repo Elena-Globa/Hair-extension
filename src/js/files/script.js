72cc0d97210134d053a111ed396a166614dd5c20
Elena-Globa <elenasachno8@gmail.com>
2025-02-04 20:06:44 +0100

    Section procedure

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -257,3 +257,216 @@ const typewriterFunc = () => {
 
 typewriterFunc ()
 
+// Показать/скрыть текст по клику
+const showTexts = () => {
+    const showTextsArray = document.querySelectorAll('[data-showTexts]');
+if (showTextsArray.length > 0) {
+  
+// Получение обычных блоков
+// Изначально я получаю коллекцию, т. к. 
+    const showTextsRegular = Array.from(showTextsArray).filter(function(item, index, self) {
+       
+        return !item.dataset.showtexts.split(",")[0];
+     
+       
+    });
+    // Инициализация обычных спойлеров
+    if (showTextsRegular.length > 0) {
+        initshowTexts(showTextsRegular);
+    }
+
+//Получение спойлеров с медиа запросами
+    const showTextsMedia = Array.from(showTextsArray).filter(function(item, index, self) {
+        return item.dataset.showtexts.split(",")[0];
+    });
+
+//Инициализация спойлеров с медиа запросами
+if (showTextsMedia.length > 0) {
+    const breakpointsArray = [];
+    showTextsMedia.forEach(item => {
+        const params = item.dataset.showtexts;
+        const breakpoint = {};
+        const paramsArray = params.split(",");
+        breakpoint.value = paramsArray[0];
+        breakpoint.type = paramsArray[1] ? paramsArray[1].trim() : "max";
+        breakpoint.item = item;
+        breakpointsArray.push(breakpoint);
+    });
+// Получаю уникальные брэйкпоинты
+    let mediaQueries = breakpointsArray.map(function(item) {
+        return '(' + item.type + "-width:" + item.value + "px)," + item.value + ',' + item.type;
+    });
+    // убираю дубли
+    mediaQueries = mediaQueries.filter(function(item, index, self) {
+        return self.indexOf(item) === index;
+    });
+// Работа с каждым брэйкпоинтом
+    mediaQueries.forEach(breakpoint => {
+        const paramsArray = breakpoint.split(",");
+        const mediaBreakpoint = paramsArray[1];
+        const mediaType = paramsArray[2];
+        const matchMedia = window.matchMedia(paramsArray[0]);
+        // Объекты с нужными условиями
+    const showTextsArray = breakpointsArray.filter(function(item) {
+    if (item.value === mediaBreakpoint && item.type === mediaType) {
+        return true;
+    }   
+});
+        // Событие
+    matchMedia.addListener(function() {
+    initshowTexts(showTextsArray, matchMedia);
+});
+initshowTexts(showTextsArray, matchMedia);
+    });
+}
+// Инициализация
+function initshowTexts(showTextsArray, matchMedia = false) {
+    showTextsArray.forEach(showTextsBlock => {
+        showTextsBlock = matchMedia ? showTextsBlock.item : showTextsBlock;
+        if (matchMedia.matches || !matchMedia) {
+            showTextsBlock.classList.add('_init');
+            initShowTextBody( showTextsBlock);
+            showTextsBlock.addEventListener("click", setShowTextAction);
+        } else {
+            showTextsBlock.classList.remove('_init');
+            initShowTextBody(showTextsBlock, false);
+            showTextsBlock.removeEventListener("click", setShowTextAction);
+        }
+    });
+}
+// Работа с контентом
+
+    function initShowTextBody(showTextsBlock, hideShowTextBody = true) {
+        const showTextTitles = showTextsBlock.querySelectorAll('[data-showtext]');
+        if (showTextTitles.length > 0) {
+            showTextTitles.forEach(showTextTitle => {  
+       
+                    console.log('lena', showTextTitle) 
+      
+                if (hideShowTextBody) { 
+                                    
+                    showTextTitle.removeAttribute('tabindex');                 
+                
+                    if (!showTextTitle.classList.contains('_active')) {
+                       
+                        showTextTitle.previousElementSibling.hidden = true;
+                                                              
+                    }
+                        }  else {    
+                            // console.log('lena1')                
+                            showTextTitle.setAttribute('tabindex', '-1');                           
+                            showTextTitle.previousElementSibling.hidden = false; 
+                                                              
+                            }
+            });
+        }
+    }
+    function setShowTextAction(e) {
+        const el = e.target;
+        if (el.hasAttribute('data-showtext') || el.closest('[data-showtext]')) {
+            const showtextTitle = el.hasAttribute('data-showtext') ? el : el.closest('[data-showtext]');
+            console.log(showtextTitle, 'showtextTitle1');
+            if (!showtextTitle.classList.contains('_active')) {
+                console.log('привет')
+                showtextTitle.innerHTML = 'Свернуть<span></span>';} else
+                 {showtextTitle.innerHTML = 'Читать далее<span></span>'}
+            const showtextsBlock = showtextTitle.closest('[data-showtext]');
+            const oneShowtext = showtextsBlock.hasAttribute('data-one-showtext') ? true : false;
+            if (!showtextsBlock.querySelectorAll('_slide').length) {
+                if (oneShowtext && !showtextTitle.classList.contains('_active')) { 
+                    console.log(showtextTitle,'showtextTitle')  
+                             
+                    hideShowtextsBody(showtextsBlock);
+
+                }
+                showtextTitle.classList.toggle('_active');             
+                // console.log(showtextTitle,'showtextTitle') 
+                _slideToggle(showtextTitle.previousElementSibling, 500);
+            }
+            e.preventDefault();            
+        }
+    }
+    function hideShowtextsBody (showtextsBlock) {
+        const showtextActiveTitle = showtextsBlock.querySelector('[data-showtext]._active');      
+        if (showtextActiveTitle) {                    
+            _slideUp(showtextActiveTitle.previousElementSibling, 500);
+        }
+    }
+
+}
+// SlideToggle
+let _slideUp = (target, duration = 500) => {
+    console.log(target, 'target');
+	if (!target.classList.contains('_slide')) {
+		target.classList.add('_slide');
+		target.style.transitionProperty = 'height, margin, padding';
+		target.style.transitionDuration = duration + 'ms';
+		target.style.height = `${target.offsetHeight}px`;
+		target.offsetHeight;
+		target.style.overflow = 'hidden';
+		target.style.height = 0;
+		target.style.paddingTop = 0;
+		target.style.paddingBottom = 0;
+		target.style.marginTop = 0;
+		target.style.marginBottom = 0;
+		window.setTimeout(() => {
+			target.hidden = true;
+			target.style.removeProperty('height');
+			target.style.removeProperty('padding-top');
+			target.style.removeProperty('padding-bottom');
+			target.style.removeProperty('margin-top');
+			target.style.removeProperty('margin-bottom');
+			target.style.removeProperty('overflow');
+			target.style.removeProperty('transition-duration');
+			target.style.removeProperty('transition-property');
+			target.classList.remove('_slide');	
+            }, duration);
+	}
+}
+let _slideDown = (target, duration = 500) => {
+	if (!target.classList.contains('_slide')) {
+		target.classList.add('_slide');
+		target.hidden = target.hidden ? false : null;
+		let height = target.offsetHeight;
+		target.style.overflow = 'hidden';
+		target.style.height = 0;
+		target.style.paddingTop = 0;
+		target.style.paddingBottom = 0;
+		target.style.marginTop = 0;
+		target.style.marginBottom = 0;
+		target.offsetHeight;
+		target.style.transitionProperty = "height, margin, padding";
+		target.style.transitionDuration = duration + 'ms';
+		target.style.height = height + 'px';
+		target.style.removeProperty('padding-top');
+		target.style.removeProperty('padding-bottom');
+		target.style.removeProperty('margin-top');
+		target.style.removeProperty('margin-bottom');
+		window.setTimeout(() => {
+			target.style.removeProperty('height');
+			target.style.removeProperty('overflow');
+			target.style.removeProperty('transition-duration');
+			target.style.removeProperty('transition-property');
+			target.classList.remove('_slide');
+			// Створюємо подію
+			// document.dispatchEvent(new CustomEvent("slideDownDone", {
+			// 	detail: {
+			// 		target: target
+			// 	}
+			// }));
+		}, duration);
+	}
+}
+let _slideToggle = (target, duration = 500) => {
+	if (target.hidden) {
+		return _slideDown(target, duration);
+	} else {
+		return _slideUp(target, duration);
+	}
+}
+}
+showTexts ()
+
+
+
+
